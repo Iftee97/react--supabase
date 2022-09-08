90d84878aac2a4f5251e5c2762bad990a45d0a0f
Iftee97 <tawsifmasrur@gmail.com>
2022-09-08 18:53:20 +0600

    added ordering by functionality and order by button styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,18 @@ import supabase from "../config/supabaseClient"
 const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState([])
+  const [orderBy, setOrderBy] = useState('created_at')
 
   const handleDelete = (id) => {
-    setSmoothies(smoothies.filter((smoothie) => smoothie.id !== id)) // filtering out the deleted smoothie from local state
+    setSmoothies(prevSmoothies => prevSmoothies.filter(smoothie => smoothie.id !== id)) // filtering out the deleted smoothie from local state
   }
 
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
         .from("smoothies")
-        .select() // fetching all rows (data) from 'smoothies' the table 
+        .select() // fetching all rows (data) from 'smoothies' the table
+        .order(orderBy, { ascending: false }) // ordering the data by the orderBy state
 
       if (error) {
         setFetchError('could not fetch smoothies')
@@ -27,7 +29,7 @@ const Home = () => {
       }
     }
     fetchSmoothies()
-  }, [])
+  }, [orderBy])
 
   return (
     <div className="page home">
@@ -35,8 +37,14 @@ const Home = () => {
       {smoothies && (
         <div className="smoothies">
           {/* order by buttons -- we'll do this later */}
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+            {/* {orderBy} */}
+          </div>
           <div className="smoothie-grid">
-            {smoothies.map(smoothie => (
+            {smoothies.map((smoothie) => (
               <SmoothieCard
                 key={smoothie.id}
                 smoothie={smoothie}
